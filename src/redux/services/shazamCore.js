7b15512d79977e63f89b0ad8afeeebf1c82d7b35
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-11-22 07:03:31 +0400

    discover page & songcard page done and fitch data from shazamcoreapi

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "50c2d37511mshb5a1869951c5cd1p161728jsn54a7122e0ca3"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;

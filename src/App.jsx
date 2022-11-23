dfb0eca3a45c6f2a059fed849dd62ef5a2a8904a
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-11-23 22:46:07 +0400

    top 5 charts & top 5 artists on home page are done

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
     <div className="relative flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <Searchbar />
+        <Searchbar/>
 
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(110vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
